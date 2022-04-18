84631b7a5ad019f96b8fcbba43356438c19994c2
SiddharthChopra28 <79740911+SiddharthChopra28@users.noreply.github.com>
2022-04-18 20:17:47 +0530

    Delete src directory

--- src/reset_dbs.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from models import User, Post, DM
-from app import db
-app = Flask(__name__, subdomain_matching=True)
-app.config.from_object('config.Config')
-db.init_app(app)
-with app.app_context():
-   db.create_all()
